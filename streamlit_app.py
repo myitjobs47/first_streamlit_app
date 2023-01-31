f59f52b405640759ea5aed55e7a004b276b1ac09
Debabrata Biswas <94610394+myitjobs47@users.noreply.github.com>
2023-01-31 12:17:45 +0000

    Update streamlit_app.py
    Emojis added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Dinner')
-streamlit.header('Breakfast Menu')
-streamlit.write('Omega 3 and Blueberry Oatmeal')
-streamlit.write('Kale, Spinach & Rocket Smoothie')
-streamlit.write('Hard-boiled Free-Range Egg')
+streamlit.title('My Mom''s New Healthy Dinner')
+streamlit.header('Breakfast Favorites')
+streamlit.write('🥣 Omega 3 and Blueberry Oatmeal')
+streamlit.write('🥤 Kale, Spinach & Rocket Smoothie')
+streamlit.write('🥚 Hard-boiled Free-Range Egg')
+streamlit.text('🥑 Avocado Toast')
+streamlit.header('🍓🍊Build Your Own Fruit Smoothie🍉🍌')
