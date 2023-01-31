720ce2675e945e441a2cc4e8bd4243ee925161fe
Debabrata Biswas <94610394+myitjobs47@users.noreply.github.com>
2023-01-31 12:22:02 +0000

    Update streamlit_app.py
    added pandas dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,12 @@
 import streamlit
+import pandas
 
 streamlit.title('My Mom''s New Healthy Dinner')
 streamlit.header('Breakfast Favorites')
 streamlit.write('🥣 Omega 3 and Blueberry Oatmeal')
 streamlit.write('🥤 Kale, Spinach & Rocket Smoothie')
 streamlit.write('🥚 Hard-boiled Free-Range Egg')
-streamlit.text('🥑 Avocado Toast')
+streamlit.write('🥑 Avocado Toast')
 streamlit.header('🍓🍊Build Your Own Fruit Smoothie🍉🍌')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
